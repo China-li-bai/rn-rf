e595d7ab7436f898e8effacc9439ea64b508cf64
f <1272679088@qq.com>
2024-07-26 16:09:55 +0800

    feat:service

---
+++ src/service/index.ts
@@ -0,0 +1,4 @@
+import { Service } from "./service";
+
+
+export const mockService =  new Service({baseURL:"http://127.0.0.1:9000"})
---
+++ src/service/service.ts
@@ -0,0 +1,56 @@
+import Taro from "@tarojs/taro";
+import { HTTP_STATUS } from "./status";
+
+interface Config extends Omit<Taro.request.Option, "url"> {
+  baseURL?: string;
+  url?: string;
+}
+export class Service {
+  instance: <T = any>(option: Config) => Taro.RequestTask<T>;
+  config: Config | undefined;
+  constructor(config?: Config) {
+    this.instance = this.request();
+    this.config = config;
+  }
+  request() {
+    return Taro.request;
+  }
+  formatOption(config) {
+    if (this.config?.baseURL) {
+      const url = this.config.baseURL + config?.url;
+      return {
+        ...this.config,
+        url,
+      };
+    }
+    return {
+      ...config,
+    };
+  }
+  async formatResult(promise) {
+    const result: Taro.request.SuccessCallbackResult<any> = await promise;
+    console.log("format", { result });
+    if (result.statusCode === HTTP_STATUS.SUCCESS) {
+      return result.data;
+    }
+    return result;
+  }
+  get(config: Config) {
+    return this.formatResult(
+      this.instance({
+        ...this.config,
+        ...this.formatOption(config),
+        method: "GET",
+      })
+    );
+  }
+  post(config: Config) {
+    return this.formatOption(
+      this.instance({
+        ...this.config,
+        ...this.formatOption(config),
+        method: "POST",
+      })
+    );
+  }
+}
